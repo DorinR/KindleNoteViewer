57899ad62d22ea7300993e390151f0bf2e8aa90c
DorinR <dorin.rogov@gmail.com>
2021-01-05 21:58:27 -0500

    #32 formatted some stuff on reset password page

--- client/src/pages/reset-password.tsx
+++ client/src/pages/reset-password.tsx
@@ -5,6 +5,8 @@ import { Wrapper } from '../components/Wrapper'
 import { InputField } from '../components/InputField'
 import { useRouter } from 'next/router'
 import { useResetPasswordMutation } from '../generated/graphql'
+import { NavBar } from '../components/NavBar'
+import { FormShaper } from '../components/FormShaper'
 
 interface ResetPasswordProps {}
 
@@ -13,26 +15,31 @@ export const ResetPassword: React.FC<ResetPasswordProps> = ({}) => {
     const [, resetPassword] = useResetPasswordMutation()
 
     return (
-        <Wrapper variant='regular'>
-            <Formik
-                initialValues={{ email: '' }}
-                onSubmit={async (values, { setErrors }) => {
-                    await resetPassword({ email: values.email })
-                    setErrors({ email: 'If the email exists, an email was sent.' })
-                    setTimeout(() => {
-                        router.push('/')
-                    }, 2000)
-                }}>
-                {({ isSubmitting }) => (
-                    <Form>
-                        <InputField name='email' label='Email' placeholder='email' />
-                        <Button mt={4} isLoading={isSubmitting} type='submit'>
-                            reset password
-                        </Button>
-                    </Form>
-                )}
-            </Formik>
-        </Wrapper>
+        <>
+            <NavBar />
+            <FormShaper>
+                <Wrapper variant='regular'>
+                    <Formik
+                        initialValues={{ email: '' }}
+                        onSubmit={async (values, { setErrors }) => {
+                            await resetPassword({ email: values.email })
+                            setErrors({ email: 'If the email exists, an email was sent.' })
+                            setTimeout(() => {
+                                router.push('/')
+                            }, 2000)
+                        }}>
+                        {({ isSubmitting }) => (
+                            <Form>
+                                <InputField name='email' label='Email' placeholder='email' />
+                                <Button mt={4} isLoading={isSubmitting} type='submit'>
+                                    reset password
+                                </Button>
+                            </Form>
+                        )}
+                    </Formik>
+                </Wrapper>
+            </FormShaper>
+        </>
     )
 }
 
